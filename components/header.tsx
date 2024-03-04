3373400fb4d4b08168f60abdb1be5d8ed7c7289d
PeiYee88 <77477618+PeiYee88@users.noreply.github.com>
2024-03-05 03:59:28 +0800

    Use clsx for conditional style formatting

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,14 @@
 
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { links } from "@/lib/data"
 import Link from "next/link";
+import clsx from "clsx";
 
 export default function Header() {
+    const [activeSection, setActiveSection] = useState('Home');
   
   return (
     <header className="z-[999] relative">
@@ -26,7 +28,10 @@ export default function Header() {
             initial={{ y: -100, opacity: 0}}
             animate={{ y: 0, opacity: 1}}>
                 <Link 
-                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition" 
+                className={clsx("flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition",
+                {
+                    "text-gray-950": activeSection === link.name
+                })}
                 href={link.hash}>
                     {link.name}
                 </Link>
--- lib/data.ts
+++ lib/data.ts
@@ -4,27 +4,27 @@ import placeholder from "@/public/my_potrait.png";
 export const links = [
     {
       name: "Home",
-      hash: "home",
+      hash: "#home",
     },
     {
       name: "About",
-      hash: "about",
+      hash: "#about",
     },
     {
       name: "Projects",
-      hash: "projects",
+      hash: "#projects",
     },
     {
       name: "Skills",
-      hash: "skills",
+      hash: "#skills",
     },
     {
       name: "Experience",
-      hash: "experience",
+      hash: "#experience",
     },
     {
       name: "Contact",
-      hash: "contact",
+      hash: "#contact",
     },
   ] as const;
 
