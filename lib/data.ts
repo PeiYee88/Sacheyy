b61a40b6e1c2f739f511d08fa03a5de48e78ded8
PeiYee88 <77477618+PeiYee88@users.noreply.github.com>
2024-03-05 01:34:07 +0800

    Mapping projects

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,5 @@
 import React from "react";
+import placeholder from "@/public/my_potrait.png"; 
 
 export const links = [
     {
@@ -24,5 +25,29 @@ export const links = [
     {
       name: "Contact",
       hash: "contact",
+    },
+  ] as const;
+
+  export const projectsData = [
+    {
+      title: "Project 1",
+      description:
+        "Placeholder",
+      tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+      imageUrl: placeholder,
+    },
+    {
+      title: "Project 2",
+      description:
+        "Placeholder",
+      tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+      imageUrl: placeholder,
+    },
+    {
+      title: "Project 3",
+      description:
+        "Placeholder",
+      tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+      imageUrl: placeholder,
     },
   ] as const;
