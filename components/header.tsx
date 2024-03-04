284f2c996c980bb3e8dbd97191857e1813299d4a
PeiYee88 <77477618+PeiYee88@users.noreply.github.com>
2024-03-05 04:06:42 +0800

    Add on click motion for header

--- components/header.tsx
+++ components/header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500
         sm:w-[initial] sm:flex-nowrap sm:gap-5">
            {links.map(link => (
-            <motion.li className="h-3/4 flex items-center justify-center" 
+            <motion.li className="relative h-3/4 flex items-center justify-center" 
             key = {link.hash}
             initial={{ y: -100, opacity: 0}}
             animate={{ y: 0, opacity: 1}}>
@@ -32,8 +32,15 @@ export default function Header() {
                 {
                     "text-gray-950": activeSection === link.name
                 })}
-                href={link.hash}>
+                href={link.hash}
+                onClick={ () => setActiveSection(link.name)}>
+
                     {link.name}
+                    {
+                        linkname === activeSection && (
+                    
+                    <span className="bg-gray-100 rounded-full absolute inset-0 -z-10"></span>
+                )}
                 </Link>
             </motion.li>
            )
