78e57f2b91c056359febefff4809d20fbbdf47b2
PeiYee88 <77477618+PeiYee88@users.noreply.github.com>
2024-03-05 22:28:57 +0800

    Add experience

--- lib/data.ts
+++ lib/data.ts
@@ -1,5 +1,8 @@
 import React from "react";
 import placeholder from "@/public/my_potrait.png"; 
+import { CgWorkAlt } from "react-icons/cg";
+import { LuGraduationCap } from "react-icons/lu";
+
 
 export const links = [
     {
@@ -55,5 +58,24 @@ export const projectsData = [
 export const skillsData = [
     "test1",
     "test2",
+] as const;
+
+export const experiencesData = [
+    {
+        title: "Graduated bootcamp",
+        location: "Miami, FL",
+        description:
+          "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+        icon: React.createElement(LuGraduationCap),
+        date: "2019",
+      },
+      {
+        title: "Front-End Developer",
+        location: "Orlando, FL",
+        description:
+          "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+        icon: React.createElement(CgWorkAlt),
+        date: "2019 - 2021",
+      },
 ] as const;
 
