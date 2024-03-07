ab464388dd9a7e2f934b3861090bfec0d2da0350
PeiYee88 <77477618+PeiYee88@users.noreply.github.com>
2024-03-08 04:33:51 +0800

    Update data

--- lib/data.ts
+++ lib/data.ts
@@ -33,49 +33,82 @@ export const links = [
 
 export const projectsData = [
 {
-    title: "Project 1",
+    title: "PennyWise",
     description:
-    "Placeholder",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    "A desktop application designed to assist users in managing their finances by facilitating budget management and providing monitoring of daily expenditures",
+    tags: ["Django", "MaterializeCSS", "MySQL"],
     imageUrl: placeholder,
 },
 {
-    title: "Project 2",
+    title: "checkUp",
     description:
-    "Placeholder",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    "An advanced database management system tailored for medical institutions to effectively maintain and monitor comprehensive patient information.",
+    tags: ["Java 11", "JUnit", "Gradle"],
     imageUrl: placeholder,
 },
 {
-    title: "Project 3",
+    title: "BnB Guru",
     description:
-    "Placeholder",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    "A machine learning project focused on predicting Airbnb prices, enabling landlords to optimize pricing strategies based on factors such as location, ratings, and other relevant features",
+    tags: ["NumPy", "Decision Forest", "Neural Network", "Scikit"],
     imageUrl: placeholder,
+},
+{
+  title: "SpongeBob Square-Manager",
+  description:
+  "A chatbot that helps users to manage their tasks efficiently",
+  tags: ["Java 11", "JUnit", "Gradle", ],
+  imageUrl: placeholder,
+},
+{
+  title: "Code Guardian",
+  description:
+  "A static program analyser that enables user to examine code at compile level",
+  tags: ["C++", "Catch2"],
+  imageUrl: placeholder,
+},
+{
+  title: "Study Buddy",
+  description:
+  "An Android application that allows students to form study dates based on their preference.",
+  tags: ["React Native", "Google Firebase", "React Redux"],
+  imageUrl: placeholder,
 },
 ] as const;
 
 export const skillsData = [
-    "test1",
-    "test2",
+    "Java",
+    "Python",
+    "Javascript/Typescript",
+    "C++",
+    "SQL",
+    "NumPy/Scikit",
+    "React",
+    "Django",
+    "Next.js",
+    "CSS/HTML",
+    "Kubernetes",
+    "Docker",
+    "AWS",
+    "Terraform",
+    "Hadoop",
 ] as const;
 
 export const experiencesData = [
-    {
-        title: "Graduated bootcamp",
-        location: "Miami, FL",
-        description:
-          "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-        icon: React.createElement(LuGraduationCap),
-        date: "2019",
-      },
       {
-        title: "Front-End Developer",
-        location: "Orlando, FL",
+        title: "DevOps Engineer Intern (Team AutoCAD)",
+        location: "Autodesk (Singapore)",
         description:
-          "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+          `
+          "During my internship, I developed automation scripts in Python to streamline data scraping from Jenkins and conducted 
+          thorough data analysis to extract valuable deployment statistics. Additionally, I 
+          implemented MongoDB and Elastic Search as the backend infrastructure, integrating
+           the project with Grafana for intuitive data visualization. This integration significantly improved overall 
+           project workflows. In the middle of my internship, I took on the responsibility of facilitating the 
+           onboarding process for teams within the AEC Division and regularly conducted sharing sessions to generate interest from additional teams."
+          `,
         icon: React.createElement(CgWorkAlt),
-        date: "2019 - 2021",
+        date: "Jan 2023 - Jun 2023",
       },
 ] as const;
 
